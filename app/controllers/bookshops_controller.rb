cc76a6230dee979177112511183c4ffb2cda6299
SHUNSUKE <s.nishino.blue.moon@gmail.com>
2019-04-28 00:19:33 +0900

    Add a picture settings

--- app/controllers/bookshops_controller.rb
+++ app/controllers/bookshops_controller.rb
@@ -24,9 +24,41 @@ class BookshopsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @shop.update_attributes(shops_params)
+      flash[:success] = "店舗の編集に成功しました。"
+      redirect_to @shop
+    else
+      flash[:danger] = "店舗の編集に失敗しました。"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @shop.destroy
+    flash[:success] = "店舗情報を削除しました。"
+    redirect_to root_url
+  end
+
   private
 
     def booksthop_params
+      params.require(:bookshop).permit(
+        :name, 
+        :phone, 
+        :image, 
+        :address, 
+        :area, 
+        :station, 
+        :url, 
+        :twiiter_url, 
+        :google_map_url, 
+        :business_hour,
+        :holiday,
+        :access)
     end
 
 end
