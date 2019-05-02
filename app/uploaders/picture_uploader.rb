97b75b4dc28986e8a42fc856c6b8b8bbfb8d9ea7
SHUNSUKE <s.nishino.blue.moon@gmail.com>
2019-05-02 18:08:27 +0900

    Add picture settings to the production environment

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,11 +3,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  # アップロードファイルの保存先ディレクトリは上書き可能
+  # 下記はデフォルトの保存先 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
