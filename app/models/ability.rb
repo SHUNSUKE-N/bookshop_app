a8fef4d4f9afcbea79ebad77da4459db5e11ec3a
SHUNSUKE <s.nishino.blue.moon@gmail.com>
2019-05-30 07:08:26 +0900

    fix-ability-setting

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,6 +12,7 @@ class Ability
         can :access, :rails_admin
         can :manage, :all
       end
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
