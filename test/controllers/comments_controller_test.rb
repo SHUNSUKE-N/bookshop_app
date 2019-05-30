fd14088380578e353409f8c88ad3e4dc380f803c
SHUNSUKE <s.nishino.blue.moon@gmail.com>
2019-05-31 07:30:42 +0900

    add-comments-test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,5 +2,13 @@ require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
 
+    def setup
+        @comment = comments(:orange)
+      end
+
+      test "should get new" do
+        get new_user_session_url
+        assert_response :success
+      end
 
 end
