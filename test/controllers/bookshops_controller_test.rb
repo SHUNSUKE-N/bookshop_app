8c6903981c397b9c667fea22408f368d57bea69c
SHUNSUKE <s.nishino.blue.moon@gmail.com>
2019-06-01 09:30:46 +0900

    add-static-test

--- test/controllers/bookshops_controller_test.rb
+++ test/controllers/bookshops_controller_test.rb
@@ -2,4 +2,14 @@ require 'test_helper'
 
 class BookshopsControllerTest < ActionDispatch::IntegrationTest
 
+    test "should get index" do
+        get bookshops_url
+        assert_response :success
+    end
+
+    test "should get new" do
+        get new_bookshop_path
+        assert_response :success
+    end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,5 +2,14 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+    test "should get new" do
+      get new_user_registration_path
+      assert_response :success
+    end
+
+    test "should get login" do
+        get new_user_session_path
+        assert_response :success
+    end
 
 end
